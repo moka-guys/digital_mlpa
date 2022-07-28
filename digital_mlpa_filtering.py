8de563251448ae21efe811a8249890fe0e26772d
ErinPeat <erin.peat@nhs.net>
2022-07-28 14:53:59 +0100

    Add scripts to repo

---
+++ digital_mlpa_filtering.py
@@ -0,0 +1,21 @@
+import digital_mlpa_filter_config as config
+import pandas as pd
+
+
+
+# 1) Import data from csv
+# challenge will be data sometimes moves... 
+# 2) filter patients based off their pan number
+# do this using a function, with row and filtering needed as an input
+# 3) save one csv per patient with filtered results
+# 4) move the old unfiltered file into a directory 
+# 5) Include debugging options 
+# 6) Include try and except statements 
+# 7) Include a check when opening to see if it's been filtered or not, somehow...
+
+
+''' ==================== SCRIPT FUNCTIONS ==================== '''
+
+
+
+'''================== Run script =========================== ''' 
