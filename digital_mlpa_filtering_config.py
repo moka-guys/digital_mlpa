f985ca0e9851812ca487167dfb4fae7614bfbb97
Erin Peat <55829630+ErinPeat@users.noreply.github.com>
2022-08-11 14:56:16 +0100

    Update digital_mlpa_filtering_config.py

--- digital_mlpa_filtering_config.py
+++ digital_mlpa_filtering_config.py
@@ -1,8 +1,17 @@
-path="/home/erin/Documents/Work/digital_mlpa/digital_mlpa/"
-
+#path="/home/erin/Documents/Work/digital_mlpa/digital_mlpa/"
+# Paths
+path = "H:/06_digital_mlpa/"
 processed_path=path+"processed/"
+logfile_path = path+"logfiles"
 
+# Expected row count to do a sense check if spreadsheet layout has changes
+expected_row_count = 594
 
+# Sample metrics values from column M of the spreadsheet
+sample_info_names = ["Median total reads", "Sample type", "Estimated DNA quantity", "SNP code",
+                                "Mutation probes detected" , "Reference probe quality", "Quality checks"]
+
+# Gene lists
 gene_list = {"Pan49" : ["APC", "BMPR1A"],
              "Pan50" : ["MLH1", "MSH2"],
              "Pan51" : ["MLH1", "BMPR1A"], 
