8aefaa67c4fc287d8f7d97c490f04a94274a657d
Erin Peat <55829630+ErinPeat@users.noreply.github.com>
2022-08-23 09:37:11 +0100

    Update as script now pulls genes from moka

--- digital_mlpa_filtering_config.py
+++ digital_mlpa_filtering_config.py
@@ -1,19 +1,13 @@
-#path="/home/erin/Documents/Work/digital_mlpa/digital_mlpa/"
+# coding: utf-8
 # Paths
-path = "H:/06_digital_mlpa/"
+#path = "P:/Bioinformatics/UAT/Julia_digital_MLPA_testing/"
+#path = "H:/06_digital_mlpa/"
 processed_path=path+"processed/"
 logfile_path = path+"logfiles"
 
-# Expected row count to do a sense check if spreadsheet layout has changes
+# Expected features to check layout is the same
 expected_row_count = 594
 
 # Sample metrics values from column M of the spreadsheet
 sample_info_names = ["Median total reads", "Sample type", "Estimated DNA quantity", "SNP code",
                                 "Mutation probes detected" , "Reference probe quality", "Quality checks"]
-
-# Gene lists
-gene_list = {"Pan49" : ["APC", "BMPR1A"],
-             "Pan50" : ["MLH1", "MSH2"],
-             "Pan51" : ["MLH1", "BMPR1A"], 
-             "Pan52" : ["EPCAM", "MUTYH", "PMS2"],
-             "Pan53" : ["PTEN", "CDKN2A", "ATM2"]  }
